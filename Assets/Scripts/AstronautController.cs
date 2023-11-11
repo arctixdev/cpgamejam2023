3b4f790348471cb3a7ba8e372f4cf51e14ee2c96
Jonathan Bangert <jonathan@bangert.dk>
2023-11-12 00:28:46 +0100

    Add astronaut controller

---
+++ Assets/Scripts/AstronautController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronautController : MonoBehaviour
+{
+    [SerializeField]
+    private int maxAstronautCount = 5;
+
+    [SerializeField]
+    private RectTransform aliveAstronautsRect;
+    [SerializeField]
+    private RectTransform deadAstronautsRect;
+
+    public int remainingAstronauts = 5;
+
+    public void resetAstronauts() {
+        remainingAstronauts = maxAstronautCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Alive astronauts
+        remainingAstronauts = Math.Max(Math.Min(maxAstronautCount, remainingAstronauts), 0);
+        aliveAstronautsRect.sizeDelta = new Vector2(100 * remainingAstronauts, 50);
+
+        // Dead astronauts
+        deadAstronautsRect.sizeDelta = new Vector2(100 * maxAstronautCount, 50);
+    }
+}
