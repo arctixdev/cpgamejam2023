26030df16efea91b7482ecad06ea9945c34b9bf4
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2023-11-12 00:16:14 +0100

    particle start

--- Assets/AstroidCollision.cs
+++ Assets/AstroidCollision.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AstroidCollision : MonoBehaviour
 {
+    public ParticleSystem asteroidParticle;
+    public LayerMask asteroidLayer; // Specify the layer on which the particle effect should be triggered
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,23 @@ public class AstroidCollision : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((asteroidLayer.value & (1 << collision.collider.gameObject.layer)) != 0)
+        {
+            asteroidParticle.Play();
+            Invoke("DisableParticleEffect", 1f);
+        }
+
+    }
+
+    void DisableParticleEffect()
+    {
+        // Stop the particle system and disable the GameObject
+        asteroidParticle.Stop();
+        gameObject.SetActive(false);
+        print("stoppedpar");
     }
 }
