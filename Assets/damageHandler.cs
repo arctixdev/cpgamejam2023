4e4a726a059848fb0b0c5bd8db1c35b19bb0fe9a
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2023-11-12 07:43:57 +0100

    Update damageHandler.cs

--- Assets/damageHandler.cs
+++ Assets/damageHandler.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class damageHandler : MonoBehaviour
 {
 
+    public string asteroid = "asteroidTag";
+
     [SerializeField]
     private healthHandler healthHandler;
+
+    [SerializeField]
+    private float astroidAttackPower;
 
     
     // Start is called before the first frame update
@@ -25,5 +30,19 @@ public class damageHandler : MonoBehaviour
     void takeDamage(float damage)
     {
         healthHandler.damage(damage);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object has the specified tag
+        if (collision.gameObject.CompareTag(asteroid))
+        {
+            // Check if the colliding object has a Rigidbody
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                takeDamage(collision.relativeVelocity.magnitude * astroidAttackPower);
+            }
+        }
     }
 }
