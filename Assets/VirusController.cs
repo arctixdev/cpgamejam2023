09a0c8f18264b9fde36716fdffabda1102a7b05e
Jonathan Bangert <jonathan@bangert.dk>
2023-11-12 10:51:45 +0100

    Remove AI code from project

--- Assets/VirusController.cs
+++ Assets/VirusController.cs
@@ -5,28 +5,20 @@ using UnityEngine;
 public class VirusController : MonoBehaviour
 {
 
-    public Transform player;
-    public float speed = 0.5f; // Adjust the speed of the movement
-    public float maxOffset = 1.0f; // Maximum random offset
+    public Transform playerTransofmr;
+    public float speed = 0.5f;
+    public float maxOffset = 1.0f;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        if (playerTransofmr != null)
         {
             float randomXOffset = Random.Range(-maxOffset, maxOffset);
             float randomYOffset = Random.Range(-maxOffset, maxOffset);
 
-            // Calculate the new position using Lerp with random offsets
-            Vector2 targetPositionWithOffset = new Vector2(player.position.x + randomXOffset, player.position.y + randomYOffset);
+            Vector2 targetPositionWithOffset = new Vector2(playerTransofmr.position.x + randomXOffset, playerTransofmr.position.y + randomYOffset);
             Vector2 newPosition = Vector2.Lerp(transform.position, targetPositionWithOffset, Time.deltaTime * speed / transform.localScale.x);
 
-            // Update the object's position
             transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
         }
     }
