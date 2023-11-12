95a599d771d8aefd935e3145d775bba2175cdbb5
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2023-11-12 02:55:53 +0100

    We Have game now

--- Assets/VirusCollision.cs
+++ Assets/VirusCollision.cs
@@ -6,15 +6,25 @@ public class VirusCollision : MonoBehaviour
 {
     // Specify the layer that should trigger destruction
     public string asteroid = "asteroidTag";
+    // Specify the minimum velocity threshold for destruction
+    public float minVelocityMagnitude = 0.1f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        // Check if the colliding object is on the specified layer
-        if (collision.gameObject.CompareTag(asteroid)) { 
-
-            // If it is on the target layer, destroy the GameObject
-            Destroy(gameObject);
+        // Check if the colliding object has the specified tag
+        if (collision.gameObject.CompareTag(asteroid))
+        {
+            // Check if the colliding object has a Rigidbody
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                // Check if the object is moving based on its velocity magnitude
+                if (rb.velocity.magnitude > minVelocityMagnitude)
+                {
+                    // If it has the target tag and is moving, destroy the GameObject
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
